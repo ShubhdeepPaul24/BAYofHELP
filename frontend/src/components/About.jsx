f2f87f4fd29c79fe08260ddcd8466623eadaf972
shubhdeeppaul71 <shubhdeeppaul71@gmail.com>
2024-04-09 16:30:40 +0530

    fyp

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+            Welcome to BayOfHelp, where compassion meets action.
+            Our platform connects donors with causes, empowering positive change.
+            Through collaboration and generosity, we strive to address pressing social issues
+             and make a difference in the lives of those in need. Join us in spreading hope and kindness today.
+            </p>
+            <Link to={"/"}>
+              Explore More{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {data} from '../restApi.json'
+const Menu = () => {
+  return (
+    <>
+      <section className='menu' id='menu'>
+        <div className="container">
+            <div className="heading_section">
+                <h1 className="heading">DONATION DRIVES</h1>
+                <p>"Join our donation drives: Together, we transform generosity into tangible impact, one contribution at a time. Let's make a difference."</p>
+            </div>
+            <div className="dishes_container">
+                {
+                    data[0].dishes.map(element => (
+                        <div className="card" key={element.id}>
+                                <img src={element.image} alt={element.title} />
+                                <h3>{element.title}</h3>
+                                <button>{element.category}</button>
+                        </div>
+                    ))
+                }   
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Menu
